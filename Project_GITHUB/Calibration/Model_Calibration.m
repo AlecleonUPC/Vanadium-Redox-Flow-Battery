32e8151b85577c369ab13e6422bae4ac970d540c
AlecleonUPC <83453954+AlecleonUPC@users.noreply.github.com>
2021-07-08 14:19:27 +0200

    Add files via upload

---
+++ Project_GITHUB/Calibration/Model_Calibration.m
@@ -0,0 +1,58 @@
+function error=Model_Calibration(x)
+global E0
+global r
+global Vtank
+global Tmeasured
+global Ctankini25
+global Ctankini34
+global Vcell
+global Ccellini25
+global Ccellini34
+global Vtank1
+global Vtank2
+global Vtank_Catholyte
+global Vtank_Anolyte
+global Tensio2
+global alpha_1
+global alpha_2
+global k0_1
+global k0_2
+
+global Vcell
+
+% E0=x(1);
+% r=x(2);
+% Vtank=x(3);
+% Ctankini25 = x(4);
+% Ctankini34 = x(5);
+% Vcell = x(6);
+% Ccellini25 = x(7);
+% Ccellini34 = x(8);
+
+
+%E0=x(1);
+%r=x(1);
+%Vtank=x(3);
+%Ctankini25 = x(1);
+%Ctankini34 = 400-Ctankini25;
+%Vcell = x(5);
+%Ccellini25 = x(2);
+%Ccellini34 = 400-Ccellini25;
+%E0=x(2);
+%[x0,a]
+
+% r = x(1);
+% E0 = x(2);
+% alpha_1 = x(3); 
+% alpha_2 = x(4);
+
+% Vcell = x(1);
+% Vtank_Anolyte = x(2);
+% Vtank_Catholyte = x(3);
+k0_1 = x(1);
+k0_2 = x(2);
+%sim('Model_Estimation');
+sim('Model');
+y=ans.Vestimated;
+
+error=sum(abs(y-Tmeasured));
---
+++ Project_GITHUB/Calibration/Results_Plots.m
@@ -0,0 +1,15 @@
+% figure
+% plot(Time2,Tensio2)
+% hold on
+% plot(Vestimated)
+% 
+% figure
+% plot(Time2,Current2)
+% hold on
+% plot(out.curr)
+
+figure(3)
+plot(Time2,Tensio2)
+hold on
+plot(out.t_1,out.Vestimated1)
+
